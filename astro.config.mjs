cf4e398a15a834e9bc69c356425fa1350f3b7435
Gavid&Doliath <valentin.fourtune@gmail.com>
2026-02-15 17:16:06 +0100

    Initial commit: Portfolio Rédactrice SEO Premium

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://VOTRE_USERNAME_GITHUB.github.io',
+  site: 'https://GavidetDoliath.github.io',
   base: '/dijehene',
   vite: {
     plugins: [tailwindcss()]
